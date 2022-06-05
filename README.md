738427868f2dd81233c66c04c8105987c5ae8bf3
yohirt <rtaraszka@poczta.fm>
2022-06-05 11:57:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rtinpw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rtinpw)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+interface Rectangle {
+  x: number;
+  y: number;
+}
+
+// type Rectangle = {
+//   x: number;
+//   y: number;
+// }
+
+// type myName = string;
+
+// interface Rectangle {
+//   (x: number, y: number) :number
+// }
+
+// type Rectangle = (x: number, y: number) => number;
+
+interface Triangle extends Rectangle {
+  z: number;
+}
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Hello {{name}}!</h1>`,
+  styles: [`h1 { font-family: Lato; }`],
+})
+export class HelloComponent implements OnInit {
+  @Input() name: string;
+
+  ngOnInit(): void {
+    let myRectangle: Rectangle;
+
+    myRectangle = (x: number, y: number) => {
+      return x * y;
+    };
+  }
+}
